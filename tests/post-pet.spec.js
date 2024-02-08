37962baf5f9deaed59f180e389ed072a69b6e030
Patricia Neris <patricia-neris@outlook.com>
2024-02-08 12:24:12 -0300

    refactore

--- tests/post-pet.spec.js
+++ tests/post-pet.spec.js
@@ -1,12 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
-// [{id: 15, username: 'maria852', firstName: 'maria', lastName: 'silva', email: 'test@mail.com', password: 'test123', phone: '11985632850', userStatus: 1}]
-
 const data = JSON.parse(JSON.stringify(require("../testdata.json")))
 
 test.describe('New Todo', () => {
   
-
   test('should allow create user by API request', async ({ request }) => {
     const newUser = await request.post(`/v2/user/createWithArray`, {data})
 
